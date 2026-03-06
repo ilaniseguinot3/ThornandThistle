5311c6ff8411d40b7cc12fecea6b02c331829200
Katzi23 <160276154+Katzi23@users.noreply.github.com>
2026-03-06 13:32:51 -0500

    "Imported door model, reimported table model, and added tanble texture.  Also added cursor and a middle mouse button toggle - press middle mouse button to rotate the camera when the mouse is moved and move the camera when WASD are pressed, if the middle mouse button is not pressed moving the mouse just moves the cursor"

--- Assets/Scripts/playerMovementScript.cs
+++ Assets/Scripts/playerMovementScript.cs
@@ -25,11 +25,16 @@ public class playerMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {        
+        // if middle mouse button is clicked, move without Cursor
         // check if mouse has moved, if it has, rotate the camera and update yaw and pitch
         if (Mouse.current == null)
             return;
-        else
+        else if (Mouse.current.middleButton.isPressed)
         {
+            // Lock the cursor to the center of the screen
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+
             Vector2 mouseDelta = Mouse.current.delta.ReadValue();
 
             yaw += mouseDelta.x * horizontalSpeed;
@@ -39,45 +44,51 @@ public class playerMovementScript : MonoBehaviour
             pitch = Mathf.Clamp(pitch, -89f, 89f);
 
             transform.rotation = Quaternion.Euler(pitch, yaw, 0f);
+
+            // get current key pressed
+            var key = Keyboard.current;
+
+            // if nothing is being pressed, return
+            if (key == null)
+                return;
+            // otherwise move accordingly
+            else 
+            {
+                // create direction vectors based on local foward and right directions
+                Vector3 facingDirection = this.transform.forward;
+                Vector3 rightDirection = this.transform.right;
+
+                // lock the Y axis
+                facingDirection.y = 0f;
+                rightDirection.y = 0f;
+
+                // Normalize the direction vectors
+                facingDirection.Normalize();
+                rightDirection.Normalize();
+
+                // make a new vector that will be used to move the character
+                Vector3 move = Vector3.zero;
+
+                // update this vector based on what key(s) are pressed
+                if(key.wKey.isPressed)
+                    move += facingDirection;
+                if(key.sKey.isPressed)
+                    move -= facingDirection;
+                if(key.aKey.isPressed)
+                    move -= rightDirection;
+                if(key.dKey.isPressed)
+                    move += rightDirection;
+
+                // actually move the camera/player
+                transform.position += move * playerSpeed * Time.deltaTime;
+                Cursor.visible = true;
+            }
         }
-
-        
-        // get current key pressed
-        var key = Keyboard.current;
-
-        // if nothing is being pressed, return
-        if (key == null)
-            return;
-        // otherwise move accordingly
-        else 
+        // otherwise just move the cursor
+        else
         {
-            // create direction vectors based on local foward and right directions
-            Vector3 facingDirection = this.transform.forward;
-            Vector3 rightDirection = this.transform.right;
-
-            // lock the Y axis
-            facingDirection.y = 0f;
-            rightDirection.y = 0f;
-
-            // Normalize the direction vectors
-            facingDirection.Normalize();
-            rightDirection.Normalize();
-
-            // make a new vector that will be used to move the character
-            Vector3 move = Vector3.zero;
-
-            // update this vector based on what key(s) are pressed
-            if(key.wKey.isPressed)
-                move += facingDirection;
-            if(key.sKey.isPressed)
-                move -= facingDirection;
-            if(key.aKey.isPressed)
-                move -= rightDirection;
-            if(key.dKey.isPressed)
-                move += rightDirection;
-
-            // actually move the camera/player
-            transform.position += move * playerSpeed * Time.deltaTime;
+            // Unlock the cursor
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 }
